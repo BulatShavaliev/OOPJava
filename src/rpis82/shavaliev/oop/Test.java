ae709f5313bd166d7e8a7f81a6108a2ec729a2c2
Bulat Shavaliev <shavaliev-bulat@mail.ru>
2020-05-13 11:17:00 +0400

    новые изменения ещё

--- src/rpis82/shavaliev/oop/Test.java
+++ src/rpis82/shavaliev/oop/Test.java
@@ -4,26 +4,6 @@ public class Test
 {
     public static void main(String[] args)
     {
-        lab1tests();
-    }
-    public static void lab1tests()
-    {
-
-    }
-    public static void lab2tests()
-    {
-
-    }
-    public static void lab3tests()
-    {
-
-    }
-    public static void lab4tests()
-    {
-
-    }
-    public static void lab5tests()
-    {
 
     }
 }
